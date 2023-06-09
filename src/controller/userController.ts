c7d0e507235dbf4d1f645b305c2612faf8f8a096
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-06-09 18:12:29 -0300

    config controller

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -4,41 +4,70 @@ import User from '../model/userModels';
 class userController{
     async create(req: Request, res: Response){
         try {
-            res.send('Hello, word')
+            const {name, email, password} = req.body;
+            const user = new User({
+                name,
+                email,
+                password
+            })
+
+            await user.save()
+            res.status(201).json(user)
         } catch (error) {
-            
+            res.status(500).send(error);
         }
         
     }
 
     async listAll(req: Request, res: Response){
         try {
-            res.send('Hello, word')
+            const user = await User.find();
+            res.json(user)
         } catch (error) {
-            
+            res.status(500).send(error);
         }
     }
 
     async search(req:Request, res: Response){
         try {
-            res.send('Hello, word')
+            const userId: string = req.params.id;
+            const user = await User.findById(userId);
+            if (!user) {
+                return res.status(404).send('User not found!');
+              }
+              res.json(user);
         } catch (error) {
-            
+            res.status(500).send(error);
         }
     }
 
     async update(req: Request, res: Response){
         try {
-            res.send('Hello, word')
+            const userId: string = req.params.id;
+            const { name, email, password }: { name: string, email: string, password: string } = req.body;
+            const updatedUser = await User.findByIdAndUpdate(
+                userId,
+                { name, email, password },
+                { new: true }
+              );
+              if (!updatedUser) {
+                return res.status(404).send('User not found!');
+              }
+              res.json(updatedUser);
         } catch (error) {
-            
+            res.status(500).send(error);
         }
     }
     async delete(req: Request, res: Response){
         try {
-            res.send('Hello, word')
+            const userId: string = req.params.id;
+            const deletedUser = await User.findByIdAndDelete(userId);
+            if (!deletedUser) {
+                return res.status(404).send('User not found!');
+              }
+              res.sendStatus(204);
         } catch (error) {
-            
+            res.status(500).send(error);
         }
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import routes from './router/index'
+import db from './dataBase/db';
+
+db.on("error", console.log.bind(console, "Erro ao conectar ao Mongo"));
+db.once("open", () => {
+  console.log("Connected successfully!");
+});
+
 const port = 3000
+
+
 
 const app = express();
 app.use(express.json());
